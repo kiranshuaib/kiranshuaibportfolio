fc22136364e6472d35590faf69e74b2d6ba61f85
kiranshuaib <kiran.shuaib@gmail.com>
2018-08-03 16:06:29 -0400

    momentum clone url added

--- script.js
+++ script.js
@@ -26,64 +26,7 @@ $(document).ready(function() {
 	var year = currentTime.getFullYear();
 
 	// data for the portfolio cards front and back
-	var projectData = [
-		{
-			title: 'Momentum Clone',
-			text:
-				'<p>A React JS project, It is the clone of the Momentum Google Chrome Extension. The extension is a home page replacement on the Chrome browser. It can be accessed by opening a new window or tab. </p> <strong>Code:</strong> ReactJS CSS<br> <strong>Features:</strong>local Time, Greetings with name, local weather, Todo, links for bookmarking, Randomized quotes, Randomized wallpaper', 
-				image: ['./images/momentum.png'],
-			site: [['Live Site', 'fa-link', 'https://chingu-voyage5.github.io/Geckos-Team-0/']],
-		},
-		{
-			title: 'Mark Down Previewer',
-			text:
-				'<p>A FreeCodeCamp project, <a href="https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer" target="_blank"><i>Build a Markdown Previewer </i></a>. The user can enter the GitHub flavored markdown into the editor, the text is rendered as HTML in the preview.</p> <strong>Code:</strong> React, Sass, babel<br> <strong>Layout:</strong> Bootstrap<br> <strong>Data:</strong> Marked library.',
-			image: ['./images/MarkdownPriewer.png'],
-			site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/pen/xzbLea']],
-		},
-		{
-			title: 'Quotes',
-			text:
-				'<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-build-a-random-quote-machine" target="_blank"><i>Build a Random Quote Machine</i></a>. The user is able to show a new random quote and send that quote to Twitter. <strong>Quotes</strong> was designed and optimized to be a mobile web app first.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br> <strong>Fonts:</strong> Font-awesome<br> <strong>Data:</strong> random famous quotes API',
-			image: ['./images/Quotes2.png'],
-			site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/full/wPQeBG/']],
-		},
-		{
-			title: 'Wiki Search',
-			text:
-				'<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-build-a-wikipedia-viewer" target="_blank"><i>Build a Wikipedia Viewer</i></a>. A user can search Wikipedia entries in a search box and see the resulting Wikipedia entries.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br><strong>Data:</strong> Wikipedia API',
-			image: ['./images/Wikipedia-Search.png'],
-			site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/full/GyjPLe/']],
-		},
-		{
-			title: 'Pomodoro Timer',
-			text:
-				'<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-build-a-pomodoro-clock" target="_blank"><i>Build a Pomodoro Clock</i></a>. A user can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed. <strong>Pomodoro Timer</strong> was designed and optimized to be a mobile web app first.</p> <strong>Code:</strong> HTML5, HTML5 Canvas, CSS3, Javascript, jQuery, ion.sound<br> <strong>Layout:</strong> Bootstrap, HTML5 Canvas<br> <strong>Fonts:</strong> IcoMoon',
-			image: ['./images/Promodoro.png'],
-			site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/full/GyjPLe/']],
-		},
-		{
-			title: 'Local Weather',
-			text:
-				'<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-show-the-local-weather" target="_blank"><i>Show the Local Weather</i></a>. A user can see the weather in my current location.. <strong>Local Weather</strong> was designed and optimized to be a mobile web app first.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br> <strong>Images:</strong> flickr.com<br> <strong>Fonts:</strong> Climacons, Open Sans<br> <strong>Data:</strong> Open Weather Map API, IP-API.com Geolocation API',
-			image: ['./images/localWeather.png'],
-			site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/pen/QayVYM']],
-		},
-		{
-			title: 'Tic-Tac-Toe',
-			text:
-				'<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/build-a-tic-tac-toe-game" target="_blank"><i>Build a Tic Tac Toe Game</i></a>. The user can play a game of Tic Tac Toe with the computer.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery',
-			image: ['./images/TicTacToe.png'],
-			site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/full/gvRxgv/']],
-		},
-		{
-			title: 'IACC IBA Alumni',
-			text:
-				'<p>Content management Website , it is IBA official website build in Wordpress. IACC is a fully responsive and dynamic webpage build in keeping in mind the SEO and mobile first approach.</br><strong>Code:</strong></br> CMS : Wordpress</br> Backend: mySql & php',
-			image: ['./images/IACC.png'],
-			site: [['Live Site', 'fa-link', 'http://ibaalumni.ca/toronto/']],
-		},
-	];
+	var projectData = [{ title: 'Momentum Clone', text: '<p>A React JS project, It is the clone of the Momentum Google Chrome Extension. The extension is a home page replacement on the Chrome browser. It can be accessed by opening a new window or tab. </p> <strong>Code:</strong> ReactJS CSS<br> <strong>Features:</strong>local Time, Greetings with name, local weather, Todo, links for bookmarking, Randomized quotes, Randomized wallpaper', image: ['./images/momentum.png'], site: [['Live Site', 'fa-link', 'https://kiranshuaib.github.io/momentum-clone/']] }, { title: 'Mark Down Previewer', text: '<p>A FreeCodeCamp project, <a href="https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer" target="_blank"><i>Build a Markdown Previewer </i></a>. The user can enter the GitHub flavored markdown into the editor, the text is rendered as HTML in the preview.</p> <strong>Code:</strong> React, Sass, babel<br> <strong>Layout:</strong> Bootstrap<br> <strong>Data:</strong> Marked library.', image: ['./images/MarkdownPriewer.png'], site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/pen/xzbLea']] }, { title: 'Quotes', text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-build-a-random-quote-machine" target="_blank"><i>Build a Random Quote Machine</i></a>. The user is able to show a new random quote and send that quote to Twitter. <strong>Quotes</strong> was designed and optimized to be a mobile web app first.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br> <strong>Fonts:</strong> Font-awesome<br> <strong>Data:</strong> random famous quotes API', image: ['./images/Quotes2.png'], site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/full/wPQeBG/']] }, { title: 'Wiki Search', text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-build-a-wikipedia-viewer" target="_blank"><i>Build a Wikipedia Viewer</i></a>. A user can search Wikipedia entries in a search box and see the resulting Wikipedia entries.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br><strong>Data:</strong> Wikipedia API', image: ['./images/Wikipedia-Search.png'], site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/full/GyjPLe/']] }, { title: 'Pomodoro Timer', text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-build-a-pomodoro-clock" target="_blank"><i>Build a Pomodoro Clock</i></a>. A user can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed. <strong>Pomodoro Timer</strong> was designed and optimized to be a mobile web app first.</p> <strong>Code:</strong> HTML5, HTML5 Canvas, CSS3, Javascript, jQuery, ion.sound<br> <strong>Layout:</strong> Bootstrap, HTML5 Canvas<br> <strong>Fonts:</strong> IcoMoon', image: ['./images/Promodoro.png'], site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/full/GyjPLe/']] }, { title: 'Local Weather', text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-show-the-local-weather" target="_blank"><i>Show the Local Weather</i></a>. A user can see the weather in my current location.. <strong>Local Weather</strong> was designed and optimized to be a mobile web app first.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br> <strong>Images:</strong> flickr.com<br> <strong>Fonts:</strong> Climacons, Open Sans<br> <strong>Data:</strong> Open Weather Map API, IP-API.com Geolocation API', image: ['./images/localWeather.png'], site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/pen/QayVYM']] }, { title: 'Tic-Tac-Toe', text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/build-a-tic-tac-toe-game" target="_blank"><i>Build a Tic Tac Toe Game</i></a>. The user can play a game of Tic Tac Toe with the computer.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery', image: ['./images/TicTacToe.png'], site: [['CodePen', 'fa-codepen', 'https://codepen.io/kiranshuaib/full/gvRxgv/']] }, { title: 'IACC IBA Alumni', text: '<p>Content management Website , it is IBA official website build in Wordpress. IACC is a fully responsive and dynamic webpage build in keeping in mind the SEO and mobile first approach.</br><strong>Code:</strong></br> CMS : Wordpress</br> Backend: mySql & php', image: ['./images/IACC.png'], site: [['Live Site', 'fa-link', 'http://ibaalumni.ca/toronto/']] }];
 
 	// add listener to all the cards for click flipping
 	function addListener() {
